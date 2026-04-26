4171aade1d4bf751882b11289724367e4bd74338
Nico6-93 <43955322+Nico6-93@users.noreply.github.com>
2026-04-26 15:07:29 -0300

    Se agregó una lista básica de reptiles con su estructura

---
+++ data/reptiles.js
@@ -0,0 +1,47 @@
+const reptiles = [
+  {
+    id: "yarara",
+    nombre: "Yarará",
+    nombreCientifico: "Bothrops pubescens",
+    tipo: "Ofidio",
+    peligrosa: true,
+    descripcion: "Serpiente venenosa presente en Uruguay. Responsable de la mayoría de los accidentes ofídicos.",
+    imagen: "img/yarara.jpg"
+  },
+  {
+    id: "coral",
+    nombre: "Serpiente Coral",
+    nombreCientifico: "Micrurus altirostris",
+    tipo: "Ofidio",
+    peligrosa: true,
+    descripcion: "Serpiente venenosa de colores llamativos. Posee veneno neurotóxico.",
+    imagen: "img/coral.jpg"
+  },
+  {
+    id: "musurana",
+    nombre: "Musurana marrón",
+    nombreCientifico: "Paraphimophis rusticus",
+    tipo: "Ofidio",
+    peligrosa: false,
+    descripcion: "Culebra inofensiva y dócil que se alimenta principalmente de oficios y roedores, matando por constricción",
+    imagen: "img/musurana.jpg"
+  },
+  {
+    id: "lagarto_overo",
+    nombre: "Lagarto overo",
+    nombreCientifico: "Salvator merianae",
+    tipo: "Saurio",
+    peligrosa: false,
+    descripcion: "Lagarto grande muy común en Uruguay. Omnívoro y generalmente inofensivo.",
+    imagen: "img/lagarto.jpg"
+  },
+  {
+    id: "tortuga",
+    nombre: "Tortuga de agua",
+    nombreCientifico: "Trachemys dorbigni",
+    tipo: "Quelonio",
+    peligrosa: false,
+    descripcion: "Tortuga de agua dulce presente en ríos y lagunas.",
+    imagen: "img/tortuga.jpg"
+  }
+];
