0a21d06c3cb83c68e1804506fcc8577a57ce55be
Nico6-93 <43955322+Nico6-93@users.noreply.github.com>
2026-04-26 21:22:51 -0300

    Modificado el atributo img para aceptar arrays

--- data/reptiles.js
+++ data/reptiles.js
@@ -6,7 +6,7 @@ const reptiles = [
     tipo: "Ofidio",
     peligrosa: true,
     descripcion: "Serpiente venenosa presente en Uruguay. Responsable de la mayoría de los accidentes ofídicos.",
-    imagen: "img/yarara.jpg"
+    imagenes: ["img/yarara.jpg", "img/yarara2.jpg", "img/yarara3.webp"]
   },
   {
     id: "coral",
@@ -15,7 +15,7 @@ const reptiles = [
     tipo: "Ofidio",
     peligrosa: true,
     descripcion: "Serpiente venenosa de colores llamativos. Posee veneno neurotóxico.",
-    imagen: "img/coral.gif"
+    imagenes: ["img/coral.gif", "img/coral2.jpg", "img/coral3.jpg"]
   },
   {
     id: "musurana",
@@ -24,7 +24,7 @@ const reptiles = [
     tipo: "Ofidio",
     peligrosa: false,
     descripcion: "Culebra inofensiva y dócil que se alimenta principalmente de oficios y roedores, matando por constricción",
-    imagen: "img/musurana.jpg"
+    imagenes: ["img/musurana.jpg", "img/musurana2.jpg", "img/musurana3.webp"]
   },
   {
     id: "lagarto_overo",
@@ -33,15 +33,60 @@ const reptiles = [
     tipo: "Saurio",
     peligrosa: false,
     descripcion: "Lagarto grande muy común en Uruguay. Omnívoro y generalmente inofensivo.",
-    imagen: "img/overo.jpg"
+    imagenes: ["img/overo.jpg", "img/overo2.jpg", "img/overo3.jpg"]
   },
   {
-    id: "tortuga",
-    nombre: "Tortuga de agua",
+    id: "morroyo",
+    nombre: "Tortuga Morrocoyo",
     nombreCientifico: "Trachemys dorbigni",
     tipo: "Quelonio",
     peligrosa: false,
     descripcion: "Tortuga de agua dulce presente en ríos y lagunas.",
-    imagen: "img/morrocoyo.webp"
-  }
+    imagenes: ["img/morrocoyo.webp", "img/morrocoyo2.jpg", "img/morrocoyo3.jpg"]
+  },
+  {
+    id: "campanita",
+    nombre: "Tortuga campanita",
+    nombreCientifico: "Phrynops hilarii",
+    tipo: "Quelonio",
+    peligrosa: false ,
+    descripcion: "La tortuga campanita es una especie dulceacuícola nativa de Sudamérica (Uruguay, Argentina, Brasil, Paraguay), destacada por ser la más grande de agua dulce en la región, pudiendo superar los 40 cm",
+    imagenes: ["img/campanita.jpg", "img/campanita2.webp", "img/campanita3.jpg"]
+  },
+  {
+    id: "canaleta",
+    nombre: "Tortuga de canaleta",
+    nombreCientifico: "Acanthochelys spixii",
+    tipo: "Quelonio",
+    peligrosa: false,
+    descripcion: "La tortuga de canaleta es una especie dulceacuícola nativa de Uruguay, caracterizada por tubérculos espinosos en su cuello y una hendidura dorsal en su caparazón. Mide hasta 20 cm, prefiere lagunas someras y es considerada una especie prioritaria para la conservación (amenazada). Se distribuye principalmente al este y norte, incluyendo Rocha, Maldonado, Rivera y Tacuarembó.",
+    imagenes: ["img/canaleta.webp", "img/canaleta2.jpg", "img/canaleta3.jpg"]
+  },
+  {
+    id: "cascabel",
+    nombre: "Vivora de cascabel",
+    nombreCientifico: "Crotalus durissus terrificus",
+    tipo: "Ofidio",
+    peligrosa: true,
+    descripcion: "La víbora de cascabel es una de las cuatro serpientes venenosas de Uruguay, caracterizada por su cascabel caudal, color castaño grisáceo con rombos oscuros y una longitud que puede superar 1.5 m. Se considera una especie amenazada y protegida por ley, con avistamientos principalmente en áreas como el Valle del Lunarejo. ",
+    imagenes: ["img/cascabel.webp", "img/cascabel2.jpg", "img/cascabel3.jpg"]
+  },
+  {
+    id: "parejera",
+    nombre: "Parejera",
+    nombreCientifico: "Philodryas patagoniensis",
+    tipo: "Ofidio",
+    peligrosa: false,
+    descripcion: "La parejera, o culebra parda, es una serpiente muy abundante y distribuida en todo Uruguay, inofensiva para los humanos pero de carácter defensivo. Mide hasta 1,8 metros, es ágil, diurna y se alimenta de roedores y otras serpientes venenosas (como la yarará), siendo aliada de la gente de campo.",
+    imagenes: ["img/parejera.jpg", "img/parejera2.jpg", "img/parejera3.webp"]
+  },
+  {
+    id: "peñarol",
+    nombre: "Culebra de peñarol",
+    nombreCientifico: "rythrolamprus poecilogyrus sublineatus",
+    tipo: "Ofidio",
+    peligrosa: false,
+    descripcion: "La culebra de Peñarol, también conocida en Argentina como verdinegra, es una serpiente inofensiva y muy común en Uruguay. Se caracteriza por sus franjas amarillas y negras que recuerdan al club de fútbol, es de hábitos diurnos/nocturnos y se alimenta de anfibios. No es venenosa ni peligrosa para personas o mascotas",
+    imagenes: ["img/peñarol.jpg", "img/peñarol2.jpg", "img/peñarol3.webp"]
+  },
 ];
